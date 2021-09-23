f40461f60512a70d69fb69c4bc09c35f7971252a
xstupk04 <tomas.stupka@centrum.cz>
2021-09-23 11:53:11 +0200

    cv1_2

--- Led_blink/Src/main.c
+++ Led_blink/Src/main.c
@@ -18,6 +18,7 @@
  */
 
 #include <stdint.h>
+#include <stm32f0xx.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -25,6 +26,20 @@
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+
+
+	uint32_t sosseq = 0b10101001110111011100101010000000;
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+	for(;;)
+	{
+		for(uint32_t i = 0; i<32; i++)
+		{
+			if (sosseq & (1U<<(31-i))){GPIOA->BSRR = (1<<5);}
+			else {GPIOA->BRR = (1<<5);}
+			for (volatile uint32_t i=0; i<100000;i++){}
+		}
+	}
 }
